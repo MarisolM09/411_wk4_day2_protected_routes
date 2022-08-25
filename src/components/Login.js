ee87fc4ee65f3eebb65481c380f2fc4ebe578fdc
MarisolM09 <mmendez9980@gmail.com>
2022-08-24 21:24:37 -0500

    finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
   const login = (e) => {
     e.preventDefault();
     // set cookie here
-    // set loggedIn = true and max-age = 60*1000 (one minute)
+    document.cookie = "loggedIn=true;Max-Age=60*1000"
 
     navigate("/");
   };
